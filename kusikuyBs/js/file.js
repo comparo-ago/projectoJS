05cd3dc7b37c02b22b4dc3efb502eced9c62f386
Matias Agobian <comparo.agobian@gmail.com>
2021-03-22 22:21:19 -0300

    camobios en js

--- kusikuyBs/js/file.js
+++ kusikuyBs/js/file.js
@@ -110,9 +110,104 @@ function mostrarAlert(){
 // agregar animaciones 
 
 $("#tituloForm").prepend('<h3>BIENVENIDOS!!!</h3>');
-$("h3").fadeOut(10000);
 
-let monthNames = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto',
+let monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September','October', 'November', 'December'];
+
+let currentDate = new Date();
+let currentDay = currentDate.getDate();
+let monthNumber = currentDate.getMonth();
+let currentYear = currentDate.getFullYear();
+
+let dates = document.getElementById('dates');
+let month = document.getElementById('month');
+let year = document.getElementById('year');
+
+let prevMonthDOM = document.getElementById('prev-month');
+let nextMonthDOM = document.getElementById('next-month');
+
+month.textContent = monthNames[monthNumber];
+year.textContent = currentYear.toString();
+
+prevMonthDOM.addEventListener('click', ()=>lastMonth());
+nextMonthDOM.addEventListener('click', ()=>nextMonth());
+
+
+
+const writeMonth = (month) => {
+
+    for(let i = startDay(); i>0;i--){
+        dates.innerHTML += ` <div class="calendar__date calendar__item calendar__last-days">
+            ${getTotalDays(monthNumber-1)-(i-1)}
+        </div>`;
+    }
+
+    for(let i=1; i<=getTotalDays(month); i++){
+        if(i===currentDay) {
+            dates.innerHTML += ` <div class="calendar__date calendar__item calendar__today">${i}</div>`;
+        }else{
+            dates.innerHTML += ` <div class="calendar__date calendar__item">${i}</div>`;
+        }
+    }
+}
+
+const getTotalDays = month => {
+    if(month === -1) month = 11;
+
+    if (month == 0 || month == 2 || month == 4 || month == 6 || month == 7 || month == 9 || month == 11) {
+        return  31;
+
+    } else if (month == 3 || month == 5 || month == 8 || month == 10) {
+        return 30;
+
+    } else {
+
+        return isLeap() ? 29:28;
+    }
+}
+
+const isLeap = () => {
+    return ((currentYear % 100 !==0) && (currentYear % 4 === 0) || (currentYear % 400 === 0));
+}
+
+const startDay = () => {
+    let start = new Date(currentYear, monthNumber, 1);
+    return ((start.getDay()-1) === -1) ? 6 : start.getDay()-1;
+}
+
+const lastMonth = () => {
+    if(monthNumber !== 0){
+        monthNumber--;
+    }else{
+        monthNumber = 11;
+        currentYear--;
+    }
+
+    setNewDate();
+}
+
+const nextMonth = () => {
+    if(monthNumber !== 11){
+        monthNumber++;
+    }else{
+        monthNumber = 0;
+        currentYear++;
+    }
+
+    setNewDate();
+}
+
+const setNewDate = () => {
+    currentDate.setFullYear(currentYear,monthNumber,currentDay);
+    month.textContent = monthNames[monthNumber];
+    year.textContent = currentYear.toString();
+    dates.textContent = '';
+    writeMonth(monthNumber);
+}
+
+writeMonth(monthNumber); 
+
+
+/*let monthNames = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto',
 'Septiembre','Octubre','Noviembre','Diciembre'];
 
 let currentDate = new Date();
@@ -121,24 +216,31 @@ let monthNumber = currentDate.getMonth();
 let currentYear = currentDate.getFullYear();
 
 let dates = document.getElementById('dates');
-let months = document.getElementById('month');
-let years = document.getElementById('year');
+let month = document.getElementById('month');
+let year = document.getElementById('year');
 
 let prevMonthDOM = document.getElementById('prev-month');
 let nextMonthDOM = document.getElementById('next-month');
 
 month.textContent = monthNames[monthNumber];
-years.textContent = currentYear.toString();
+year.textContent = currentYear.toString();
 
 prevMonthDOM.addEventListener('click',()=>lastMonth());
 nextMonthDOM.addEventListener('click',()=>nextMonth());
 
+writeMonth(monthNumber);
 
 function writeMonth(month){
+    for(let i = startDay(); i>0; i--){
+        dates.innerHTML += `<div class= "calendar__date calendar__item">${i}
+        ${getTotalDays(monthNumber-1)-(i-1)}
+        </div>`;
+    }
+
   for(let i = 1; i <=getTotalDays(month); i++){
       
     if(i===currentDay){
-    dates.innerHTML += `<div class= "calendar__date calendar__item callendar__today">${i} </div>`;
+    dates.innerHTML += `<div class= "calendar__date calendar__item calendar__today">${i} </div>`;
     }else{
         dates.innerHTML += `<div class= "calendar__date calendar__item">${i} </div>`;
     }
@@ -184,4 +286,6 @@ function setNewDate(){
     currentDate.setFullYear(currentYear,monthNumber,currentDay);
     month.textContent = monthNames[monthNumber];
     year.textContent = currentYear.toString();
-}
+    dates.textContent = '';
+    writeMonth(monthNumber);
+}*/
